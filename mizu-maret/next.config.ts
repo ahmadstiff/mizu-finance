14767712bc8a80864d7b60d22c65098ab0050190
ahmadstiff <royanuahmad313@gmail.com>
2025-05-05 10:27:22 +0700

    fix config

--- mizu-maret/next.config.ts
+++ mizu-maret/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["ipfs.io","res.cloudinary.com","https://encrypted-tbn0.gstatic.com"],
+    domains: ["ipfs.io","res.cloudinary.com","encrypted-tbn0.gstatic.com"],
   },
 };
 
