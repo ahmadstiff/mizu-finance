f6b14342675736052c98e27b540f8b274935ae21
ahmadstiff <royanuahmad313@gmail.com>
2025-05-06 18:18:46 +0700

    fix address

--- mizu-maret/src/constants/addresses.ts
+++ mizu-maret/src/constants/addresses.ts
@@ -2,9 +2,9 @@ import { Address } from "viem";
 
 export const property = "0x58d9f80f107A3E6042d70859271B277C5ba6C392" as Address;
 export const musicRoyalities = "0x96FD9ED546Bb6be177Cc86E7C31499E3C6BCA955" as Address;
-export const dlt = "0xf1CBd3F81130CaFc51E0efF03c43Fc9323eED17f" as Address;
-export const mizuMarketplace = "0xd8Ed092e21886834C9eE671b5f8040F02f9FbFa0" as Address;
-export const mockUsdc = "0x19f2Bec648b24347B49f7F53b3b9a91939b5FDC6" as Address;
-export const mockUsdt = "0xAD9A2A9352Ec396cf7ad9518Ee5F4AA6AD80E3cC" as Address;
-export const mockPaxg = "0xb6C2aFB475F1400428A0DC73C72ed1BC9eec4aF8" as Address;
+export const dlt = "0x14f49fE8be2Cf8a6Cb97e8274c8Ca7390b7A0563" as Address;
+export const mizuMarketplace = "0xb7e1aC2AECB6011BcF5f4965486f154d45aa2E06" as Address;
+export const mockUsdc = "0xfC01fBfa55586E6d00fDB91365628Bf231982f4F" as Address;
+export const mockUsdt = "0x4a965154801fB06730B0d24B24E6cBb952a37BEd" as Address;
+export const mockPaxg = "0xA8D56CC8C99173921dd9bAC3dBa2A1673200b601" as Address;
 export const hxAddress = "https://optimism-sepolia.blockscout.com/address/" as Address;
